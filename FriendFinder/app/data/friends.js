6a6159920c44dc581547685a99eee4677dd882bf
atharvab94 <36422456+atharvab94@users.noreply.github.com>
2018-04-12 17:24:10 -0500

    Add files via upload

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [
+	{
+		"name": "Taylor Swift",
+		"photo": "http://content.hollywire.com/sites/default/files/styles/main_image_teaser/public/2015/08/06/taylor-swift-2015-tour-dates.jpg?itok=SU3zFeCZ",
+		"scores": [1,1,1,1,1,1,1,1,1,1]
+		
+	},
+	{
+		"name": "snoop dogg",
+		"photo": "https://pbs.twimg.com/profile_images/943933166015803392/jvjasD7v_400x400.jpg",
+		"scores": [5,5,1,5,5,5,5,5,5,5]
+		
+	}
+];
+
+
+module.exports = friends;
